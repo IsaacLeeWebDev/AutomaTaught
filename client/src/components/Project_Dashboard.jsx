4007ae5a591e4945e5ca337f772e101729e8ff16
IsaacLeeWebDev <isaacleewebdev@gmail.com>
2018-12-02 20:12:27 -0800

    Set up files for finalized view structure

---
+++ client/src/components/Project_Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Projects_List_Item from './Projects_List_Item.jsx';
+import {
+  Row, Col, Well, Button,
+} from 'react-bootstrap';
+
+const Projects_List = (props) => (
+  <div>Hello From Project Dashboard!</div>
+);
+
+export default Projects_List;
+
