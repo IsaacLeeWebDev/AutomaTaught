54d32b8e0043116d975c8dab16e2dc8a644e19ac
IsaacLeeWebDev <isaacleewebdev@gmail.com>
2018-12-03 15:10:01 -0800

    add files to split ProjectsForm into a wizard

---
+++ client/src/components/Project_Form_Step_1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import $ from 'jquery';
+import {
+  Row, Col, Form, FormGroup, FormControl, ControlLabel, Well
+} from 'react-bootstrap';
+
+const Project_Form_Step_1.jsx = props => {
+
+}
