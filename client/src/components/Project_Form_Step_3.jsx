608fa14f2bd4412964bc7da6ee1a70237d8da9a7
IsaacLeeWebDev <isaacleewebdev@gmail.com>
2018-12-03 16:35:33 -0800

    detail requirements for Project_Form_Step_3.jsx

--- client/src/components/Project_Form_Step_3.jsx
+++ client/src/components/Project_Form_Step_3.jsx
@@ -22,3 +22,41 @@ const Project_Form_Step_3 = props => (
 );
 
 export default Project_Form_Step_3;
+
+
+/*
+
+  - [ ] Detail the tech choices and datashape
+    - [ ] What tech choices the user makes
+      - [ ] Database Selection
+        - [ ] Ask whether or not bad read/write consistency would result in imprisonment
+          - [ ] If so, ask whether or not they know Postgres
+            - [ ] If so, they get Postgres
+            - [ ] If not, they get MySQL
+          - [ ] If not, they get MongoDB
+    - [ ] Primary Record:
+      - [ ] Add columns
+        - [ ] Name:
+        - [ ] Datatype: (one of int, string, date, boolean, or secondary record)
+          - [ ] if secondary record
+            - [ ] Like a List of things, or Like an object with properties?
+            - [ ] recurse Add columns
+      - [ ] should create a json object representing a primary record.
+      - [ ] How is primary record created?
+        - [ ] Submiting a form which belongs to a unique view
+        - [ ] Triggering a non-form-related event
+      - [ ] Should app have a homepage? (+1 view)
+        - [ ] views.push('homepage_view)')
+      - [ ] Where is the primary record displayed?
+        - [ ] On the homepage
+        - [ ] in a list, like a feed or list of profiles
+          - [ ] views.push(list_name)
+        - [ ] in its own view, like a map search result
+          - [ ] views.push(list_item_name)
+        - [ ] list & view but not homepage
+          - [ ] views.push(list_name, list_item_name)
+        - [ ] in a wizard
+          - [ ] How many views/pages in the wizard?
+          - [ ] veiws.concat([...wizard-pages/veiws])
+
+*/
