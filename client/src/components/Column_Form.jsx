07214597ca7aba1013724531cad643a83d1db000
IsaacLeeWebDev <isaacleewebdev@gmail.com>
2018-12-03 19:56:12 -0800

    draft up Column Form components

--- client/src/components/Column_Form.jsx
+++ client/src/components/Column_Form.jsx
@@ -1,8 +1,65 @@
-    </FormGroup>
-    <FormGroup controlId="primaryRecordColumns">
-      <h3>Primary Record Columns</h3>
+// Packages
+import React from 'react';
+import $ from 'jquery';
+import {
+  Button, Radio, Row, Col, Form, FormGroup, FormControl, ControlLabel, Well
+} from 'react-bootstrap';
+
+const Column_Form = props => (
+  <Row>
+    <h4> Column 0 </h4>
+    <FormGroup controlId="column_0_name">
       <Col componentClass={ControlLabel} sm={4}>Column Name</Col>
       <Col sm={8}>
-        <FormControl id="columnName_1" className="form_field" type="datetime-local" />
+        <FormControl id="column_0_name" className="form_field" type="text" />
       </Col>
     </FormGroup>
+  </Row>
+  <Row>
+    <FormGroup controlId="column_0_datatype">
+      <Col componentClass={ControlLabel} sm={4}>Column Type</Col>
+      <Col sm={8}>
+        <Radio name="column_0_datatype" inline>
+          Value
+        </Radio>{' '}
+        <Radio name="column_0_datatype" inline>
+          Secondary Column
+        </Radio>{' '}
+      </Col>
+    </FormGroup>
+  </Row>
+  <Row>
+    <FormGroup controlId="column_0_created_on">
+      <Col componentClass={ControlLabel} sm={4}>Created On</Col>
+      <Col sm={8}>
+        <Radio name="column_0_created_on" inline>
+          Homepage
+        </Radio>{' '}
+        <Radio name="column_0_created_on" inline>
+          Its own view
+        </Radio>{' '}
+      </Col>
+    </FormGroup>
+  </Row>
+  <Row>
+    <FormGroup controlId="column_0_displayed_on">
+      <Col componentClass={ControlLabel} sm={4}>Displayed on</Col>
+      <Col sm={8}>
+        <Radio name="column_0_displayed_on" inline>
+          Homepage
+        </Radio>{' '}
+        <Radio name="column_0_displayed_on" inline>
+          Its own view
+        </Radio>{' '}
+        <Radio name="column_0_displayed_on" inline>
+          Both
+        </Radio>{' '}
+      </Col>
+    </FormGroup>
+  </Row>
+  <Row>
+    <Button> Add Column </Button>
+  </Row>
+);
+
+export default Column_Form_List;
---
+++ client/src/components/Column_Form_List.jsx
@@ -0,0 +1,12 @@
+// Packages
+import React from 'react';
+import $ from 'jquery';
+import {
+  Button, Radio, Row, Col, Form, FormGroup, FormControl, ControlLabel, Well
+} from 'react-bootstrap';
+// Components
+import Column_Form from 'Column_Form.jsx';
+
+const Column_Form_List = props => [];
+
+export default Column_Form_List;
